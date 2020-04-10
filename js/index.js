4a5899a008545b373ed74aa126d97779ed630aaa
ostap-02449013 <56288568+ostap-02449013@users.noreply.github.com>
2020-04-10 17:33:31 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 insert = num => document.form.textview.value += num;
 equal = () => document.form.textview.value = eval(document.form.textview.value);
 clean = () => document.form.textview.value = '';
-back = () => {
-    let exp = document.form.textview.value;
-    document.form.textview.value = exp.substring(0, exp.length - 1);
-}
+back = () => {let exp = document.form.textview.value;document.form.textview.value = exp.substring(0, exp.length - 1);}
